f4016fa4f319a0e78e83f2ed39401d92c0c0589e
Parvez <mohammedparvez1923@gmail.com>
2021-04-05 19:26:54 +0600

    commit 4

--- src/context/context.js
+++ src/context/context.js
@@ -21,6 +21,57 @@ class ProductProvider extends Component {
 		singleProducts: {},
 		loading: true,
 	};
+	componentDidMount() {
+		//from contentful items
+		this.setProducts(items);
+	}
+
+	//set products
+
+	setProducts = (products) => {
+		let storeProducts = products.map((item) => {
+			const { id } = item.sys;
+			const image = item.fields.image.fields.file.url;
+			const product = { id, ...item.fields, image };
+
+			console.log(product);
+			return product;
+		});
+		//featuredproduct
+		let featuredProducts = storeProducts.filter(
+			(item) => item.featured === true
+		);
+
+		this.setState({
+			storeProducts,
+			filteredProducts: storeProducts,
+			featuredProducts,
+			cart: this.getStorageCart(),
+			singleProduct: this.getStorageProduct(),
+			loading: false,
+		});
+	};
+
+	getStorageCart = () => {
+		return [];
+	};
+	getStorageProduct = () => {
+		return [];
+	};
+	//getTotals
+	getTotals = () => {};
+	//addTotals
+	addTotals = () => {};
+	//sync
+	syncStorage = () => {};
+
+	addToCart = (id) => {
+		console.log(`add to cart ${id}`);
+	};
+
+	setSingleProduct = (id) => {
+		console.log(`set single product ${id}`);
+	};
 	// handle sidebar
 	handleSidebar = () => {
 		this.setState({ sidebarOpen: !this.state.sidebarOpen });
@@ -46,6 +97,7 @@ class ProductProvider extends Component {
 					handleCart: this.handleCart,
 					closeCart: this.closeCart,
 					openCart: this.openCart,
+					addToCart: this.addToCart,
 				}}
 			>
 				{this.props.children}
